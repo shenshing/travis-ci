5b892bec54b4dffee411006b80218b6cd8c9ba59
shenshing <lyshenshing20@gmail.com>
2020-05-11 23:59:47 -0400

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("Cambodia Kingdom of Wonder");
+    rocket::ignite().mount("/hello", routes![hello]).launch();
 }
